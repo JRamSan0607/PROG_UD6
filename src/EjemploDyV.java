ec1409db78f6abfea55913908deef780b41b24d0
Ramirez Sanchez, Jose <jramsan0607@g.educaand.es>
2023-03-20 12:58:48 +0100

    Subida ejercicios

---
+++ src/EjemploDyV.java
@@ -0,0 +1,14 @@
+public class EjemploDyV {
+    public static void main(String[] args) {
+        int[] a = {-2, -1, 3, 5, 8, 10};
+        int num = 5;
+        int pos = dyv(a, num);
+        System.out.println("El número " + num + "está en la posición: " + pos);
+    }
+    int num = 5;
+
+    private static int dyv(int[] a, int num) {
+        int pos = 0;
+        return pos;
+    }
+}
